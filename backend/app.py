ee191c6746acfd7bb16261554f1e4b3f88ae0b58
DonnieData <donnell.jackson@outlook.com>
2026-07-07 14:03:19 -0400

    updating app dev

--- backend/app.py
+++ backend/app.py
@@ -8,8 +8,6 @@ from dotenv import load_dotenv
 #%%
 load_dotenv()
 
-
-
 #%%
 #configure core application 
 app = Flask(__name__)
@@ -32,6 +30,60 @@ class FarmersMarket(db.Model):
     __tablename__ = 'NY_Farmers_Markets'
     __table_args__ = {'schema': 'public'}
 
-    
+    #define column mapping 
+    #varaibles are  named exactly as they are in database 
+    county = db.Column(db.String(100))
+    market_name = db.Column(db.String(300))
+    market_location = db.Column(db.Text)
+    address_line_1 = db.Column(db.Text )
+    city =  db.Column(db.String(100))
+    state =  db.Column(db.String(100))
+    zip =  db.Column(db.String(20))
+    contact = db.Column(db.String(300))
+    phone = db.Column(db.String(20))
+    market_link = db.Column(db.Text)
+    operation_hours = db.Column(db.Text)
+    operation_season = db.Column(db.Text)
+    operating_months = db.Column(db.Text)
+    fmnp = db.Column(db.String(5))
+    snap = db.Column(db.String(5))
+    fcc_issued = db.Column(db.String(5))
+    fcc_accepted = db.Column(db.String(5))
+    wic_vf = db.Column(db.String(5))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    georeference_1 = db.Column(db.Text)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    #%%
+    #FarmersMarket.__tablename__
+    #FarmersMarket.county
+#%%
+#helps map data so its intepretable and endpoint doesnt break
+#Flask APIs communicate using JSON when database is queried using SQLAlchemy, it doesn't return a dictionary/JSON; returns a SQLAlchemy Model Instance (a object with database connections attached to it)
+    def to_dict(self):
+        return {
+            "county": self.county ,
+            "market_name": self.market_name ,
+            "market_location": self.market_location ,
+            "address_line_1": self.address_line_1 ,
+            "city": self.city ,
+            "state": self.state ,
+            "zip": self.zip ,
+            "contact": self.contact ,
+            "phone": self.phone ,
+            "market_link": self.market_link ,
+            "operation_hours": self.operation_hours ,
+            "operation_season": self.operation_season ,
+            "operating_months": self.operating_months ,
+            "fmnp_accepted": self.fmnp_accepted ,
+            "snap_accepted": self.snap_accepted  ,
+            "fcc_issued": self.fcc_issued ,
+            "fcc_accepted": self.fcc_accepted ,
+            "wic_vf": self.wic_vf ,
+            "coordinates": {"lat": self.latitude, "lng": self.longitude},
+            "georeference_1": self.georeference_1 ,
+            "id": self.id 
+            }
 
 # %%
